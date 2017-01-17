3e2b63f72cc55352d990ee69133c8e517c841e1c
Administrador <Administrador@172.28.10.54>
2017-01-17 17:20:51 +0100

    sfgfg

---
+++ src/ArraySum.java
@@ -0,0 +1,45 @@
+
+
+
+
+/**
+* HACED EL CUERPO DEL MAIN
+* PARA QUE DEVUELVA LA NOTA
+* MEDIA 
+* 
+* Ojo con los tipos
+* la media será real (float)
+* 
+*/
+public class ArraySum {
+
+	private static int[] array_notas = {0, 10, 2, 6, 7, 5, 4};
+	
+	/*public double main(String[] args) {
+	
+		double media = 0.0;
+		for (int i=0; i < array_notas.length; i++) {
+			 media = media + array_notas[i];
+			 }
+		media = media / array_notas.length;
+		
+		return media;
+	}*/
+	
+	
+	public static void main(String[] args) {
+		
+		double media = 0.0;
+		for (int i=0; i < array_notas.length; i++) {
+			 media = media + array_notas[i];
+			 }
+		media = media / array_notas.length;
+		
+		System.out.println("La media es " + media);
+		
+	}
+	}
+	
+	
+	//AitorPCGR
+
