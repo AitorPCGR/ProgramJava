9af581beefd7c01f0ce21a8ed8d63578e51288fd
Administrador <Administrador@FORM-P2-A1-012>
2017-01-31 18:53:11 +0100

    ghhjjh

---
+++ src/baseDatos/EmpleadosDTO.java
@@ -0,0 +1,30 @@
+package baseDatos;
+
+public class EmpleadosDTO {
+
+	private String nombre;
+	private int salario;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getSalario() {
+		return salario;
+	}
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
